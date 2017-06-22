a6ced88a40edb53866270304fb92421d6c579a0d
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-06-22 02:11:23 -0300

    Delimiting max year to ano field

--- api/motosBR/motosBR.js
+++ api/motosBR/motosBR.js
@@ -2,9 +2,9 @@ const restful = require('node-restful')
 const mongoose = require('mongoose')
 
 const motosSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
-  marca: { type: String, required: true },
-  ano: { type: Number, min: 0, required: true }
+  nome: { type: String, required: [true, 'Informe o nome da moto'] },
+  marca: { type: String, required: [true, 'Informe a marca da moto'] },
+  ano: { type: Number, min: 0, required: [true, 'Informe o ano da moto'] }
 })
 
 module.exports = restful.model('Motos_Schema', motosSchema)
--- api/motosBR/motosBRService.js
+++ api/motosBR/motosBRService.js
@@ -2,5 +2,7 @@ const MotosBR = require('./motosBR')
 
 //Cria API REST baseada no padrão
 MotosBR.methods(['get', 'post', 'put', 'delete'])
+//Retorna objeto novo em PUT/POST
+MotosBR.updateOptions({new: true})
 
 module.exports = MotosBR
