b489ee687010307bc53819c1a67ab8d504ece8a0
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-06-22 00:38:33 -0300

    add epecific error messages

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
 //Conexão MongoDB e mapeamento dos objetos
 const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://motos_br:ti1606@ds139428.mlab.com:39428/db_motos')
+
+mongoose.Error.messages.general.required = "O atributo {PATH} é obrigatório."
