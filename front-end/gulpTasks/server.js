269a735e4e3d0c56cf399ca4eb4a0e6de698ec20
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-06-28 01:06:13 -0300

    building is done

--- front-end/gulpTasks/server.js
+++ front-end/gulpTasks/server.js
@@ -1,9 +1,23 @@
 const gulp = require('gulp')
+const watch = require('gulp-watch')
+const webserver = require('gulp-webserver')
 
-gulp.task('server', ['watch'], function () {
-
+gulp.task('server', ['watch'], function() {
+  gulp.src('public').pipe(webserver({
+    livereload: true,
+    port: 3000,
+    //Abrir browser automaticamente
+    // open: true
+  }))
 })
 
-gulp.task('watch', function () {
-
+gulp.task('watch', function() {
+  //Se modificou html
+  watch('app/**/*.html', () => gulp.start('app.html'))
+  //Se modificou css
+  watch('app/**/*.css', () => gulp.start('app.css'))
+  //Se modificou js
+  watch('app/**/*.js', () => gulp.start('app.js'))
+  //Se modificou arquivos em assets
+  watch('assets/**/*.*', () => gulp.start('app.assets'))
 })
