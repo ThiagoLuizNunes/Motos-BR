6b393f1209ff47a56482614856dfb9ea38426933
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-06-29 00:47:58 -0300

    post method added in frontend

--- front-end/app/controllers/motorcycleController.js
+++ front-end/app/controllers/motorcycleController.js
@@ -7,7 +7,17 @@
   function MotorcycleController($http) {
     const vm = this
 
+    /*Post Method*/
     vm.create = function () {
+      const url = 'http://localhost:4000/api/motorcycle/'
+      $http.post(url, vm.motorcycles).then(function (response) {
+        vm.motorcycles = {}
+        console.log('Success');
+      }).catch(function (response) {
+        alert('Error to send POST!')
+      })
     }
+
+    /**/
   }
 })()
