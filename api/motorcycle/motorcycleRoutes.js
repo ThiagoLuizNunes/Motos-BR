b044d21f284bec1322ac51c4607eceff237800a2
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-07-09 08:03:46 -0300

    taking some querys from database

--- api/motorcycle/motorcycleRoutes.js
+++ api/motorcycle/motorcycleRoutes.js
@@ -3,10 +3,5 @@ module.exports =  function (server, motorcycle) {
   server.get('/search-brand/:brand', motorcycle.searchByBrand)
   server.get('/search-cylinder/:cylinder', motorcycle.searchByCylinder)
   server.get('/search-style/:style', motorcycle.searchByStyle)
-  server.get('/search-potency/:potency', motorcycle.searchByPotency)
-  server.get('/search-torque/:torque', motorcycle.searchByTorque)
-  server.get('/search-seat/:seat', motorcycle.searchBySeat)
-  server.get('/search-weight/:weight', motorcycle.searchByWeight)
-  server.get('/search-tank/:tank', motorcycle.searchByTank)
   server.get('/search-comparison/:n1/:n2', motorcycle.searchByComparison)
 }
--- api/motorcycle/motorcycleSearch.js
+++ api/motorcycle/motorcycleSearch.js
@@ -29,7 +29,7 @@ const searchByBrand = (req, res, next) => {
 /*Search method by cylinder*/
 const searchByCylinder = (req, res, next) => {
   const cylinder = req.params.cylinder
-  Motorcycle.find({'cylinder' : cylinder }, (err, motorcycle) => {
+  Motorcycle.find({'category' : cylinder }, (err, motorcycle) => {
     if (err) {
       return handleError(err)
     }
@@ -43,71 +43,6 @@ const searchByCylinder = (req, res, next) => {
 const searchByStyle = (req, res, next) => {
   const style = req.params.style
   Motorcycle.find({'style' : style }, (err, motorcycle) => {
-    if (err) {
-      return handleError(err)
-    }
-    else {
-      res.json(motorcycle)
-    }
-  })
-}
-
-/*Search method by potency*/
-const searchByPotency = (req, res, next) => {
-  const potency = req.params.potency
-  Motorcycle.find({'potency' : potency }, (err, motorcycle) => {
-    if (err) {
-      return handleError(err)
-    }
-    else {
-      res.json(motorcycle)
-    }
-  })
-}
-
-/*Search method by torque*/
-const searchByTorque = (req, res, next) => {
-  const torque = req.params.torque
-  Motorcycle.find({'torque' : torque }, (err, motorcycle) => {
-    if (err) {
-      return handleError(err)
-    }
-    else {
-      res.json(motorcycle)
-    }
-  })
-}
-
-/*Search method by seat*/
-const searchBySeat = (req, res, next) => {
-  const seat = req.params.seat
-  Motorcycle.find({'seat' : seat }, (err, motorcycle) => {
-    if (err) {
-      return handleError(err)
-    }
-    else {
-      res.json(motorcycle)
-    }
-  })
-}
-
-/*Search method by weight*/
-const searchByWeight = (req, res, next) => {
-  const weight = req.params.weight
-  Motorcycle.find({'weight' : weight }, (err, motorcycle) => {
-    if (err) {
-      return handleError(err)
-    }
-    else {
-      res.json(motorcycle)
-    }
-  })
-}
-
-/*Search method by tank*/
-const searchByTank = (req, res, next) => {
-  const tank = req.params.tank
-  Motorcycle.find({'tank' : tank }, (err, motorcycle) => {
     if (err) {
       return handleError(err)
     }
@@ -131,6 +66,5 @@ const searchByComparison = (req, res, next) => {
   })
 }
 
-module.exports = { searchByBrand, searchByCylinder, searchByStyle, searchByPotency,
-                   searchByTorque, searchBySeat, searchByWeight, searchByTank,
+module.exports = { searchByBrand, searchByCylinder, searchByStyle,
                    searchByComparison, searchByName }
