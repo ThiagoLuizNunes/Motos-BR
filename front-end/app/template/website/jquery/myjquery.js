d7a6441a8d9bd5040e2a7abdfe4f6225bcff29ee
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-07-14 17:39:28 -0300

    adding transition background color

--- front-end/app/template/website/jquery/myjquery.js
+++ front-end/app/template/website/jquery/myjquery.js
@@ -1,8 +1,12 @@
 $(window).scroll(function() {
-  if($(document).scrollTop() > 10) {
-    $("#mybar").css({"background-color": "rgba(0, 0, 0, 0.8)"});
+  if($(document).scrollTop() > 50) {
+    $("#mybar").css({
+      "background-color": "rgba(0, 0, 0, 0.8)",
+      "transition": "background-color 0.5s ease"
+    });
   }
   else {
     $("#mybar").css({"background-color": "rgba(0, 0, 0, 0.5)"});
+    "transition": "background-color 0.5s ease"
   }
 });
