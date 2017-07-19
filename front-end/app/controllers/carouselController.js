4e9c9ad9a7f6502af83a9ae008e2bfd09fcaa4eb
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-07-19 17:23:45 -0300

    changing initial slide

--- front-end/app/controllers/carouselController.js
+++ front-end/app/controllers/carouselController.js
@@ -6,15 +6,16 @@
 
   function CarouselController($scope) {
     const vm = this
-    this.slides = [{
-      image: '../../../../assets/imgs/fhoto6.1.jpg',
-      text: 'Faça da sua vida uma grande aventura.',
-      button: 'ENCONTRE SUA MOTO'
-    },
+    this.slides = [
     {
       image: '../../../../assets/imgs/fhoto4.1.jpg',
       text: 'Surpreenda-se com o poder do motor.',
       button: 'COMPARE AS POTÊNCIAS'
+    },
+    {
+      image: '../../../../assets/imgs/fhoto6.1.jpg',
+      text: 'Faça da sua vida uma grande aventura.',
+      button: 'ENCONTRE SUA MOTO'
     },
     {
       image: '../../../../assets/imgs/fhoto1.1.jpg',
