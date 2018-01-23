6305fad703c508787c3a3800cb382582a7e412a7
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-01-23 17:23:52 -0300

    delete useless codes

--- api/user/authService.js
+++ api/user/authService.js
@@ -49,31 +49,31 @@ const signup = (req, res, next) => {
   const keyAdmin = req.body.key || ''
 
   if(!email.match(emailRegex)) {
-    return res.status(400).send({errors: ['O e-mail informado está inválido']})
+    return res.status(400).send({errors: ['Email invalid!']})
   }
 
   if(!password.match(passwordRegex)) {
     return res.status(400).send({errors: [
-      "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$%) e tamanho entre 6-12."
+      "Password must have: a capital letter, a lowercase letter, a number, a special character (@ # $%) and size between 6-12."
       ]})
   }
 
   const salt = bcrypt.genSaltSync()
   const passwordHash = bcrypt.hashSync(password, salt)
   if(!bcrypt.compareSync(confirmPassword, passwordHash)) {
-    return res.status(400).send({errors: ['Senhas não conferem.']})
+    return res.status(400).send({errors: ['Passwords do not match.']})
   }
 
   const keyHash = bcrypt.hashSync(keyAdmin, salt)
   if(!bcrypt.compareSync(env.keyAdmin, keyHash)) {
-    return res.status(400).send({errors: ['Chave incorreta.']})
+    return res.status(400).send({errors: ['Incorrect key.']})
   }
 
   User.findOne({email}, (err, user) => {
     if(err) {
       return sendErrorsFromDB(res, err)
     } else if (user) {
-      return res.status(400).send({errors: ['Usuário já cadastrado.']})
+      return res.status(400).send({errors: ['User already registered!']})
     } else {
       const newUser = new User({ name, email, password: passwordHash })
       newUser.save(err => {
@@ -87,4 +87,20 @@ const signup = (req, res, next) => {
   })
 }
 
-module.exports = { login, signup, validateToken }
+const forgotPassword = (req, res, next) => {
+  const name = req.body.name || ''
+
+  if(!email.match(emailRegex)) {
+    return res.status(400).send({errors: ['Email invalid!']})
+  }
+
+  User.findOne({email}, (err, user) => {
+    if (err) {
+      return sendErrorsFromDB(res, err)
+    } else {
+
+      return res.status(200).send('Password reseted!')
+    }
+  })
+}
+module.exports = { login, signup, validateToken, forgotPassword }
